3330af07eb3ec225df5d1dd3410f520ce94dcd27
wahid_9000 <wahidchowdhury9000@gmail.com>
2026-09-15 17:31:19 +0200

    feat(auth): update auth client to use environment variable for base URL

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,5 +2,5 @@ import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
   // baseURL: "https://blog-application-ten-omega.vercel.app"
-  baseURL: "http://localhost:5000",
+  baseURL: process.env.SERVER_API || "http://localhost:5000",
 });
